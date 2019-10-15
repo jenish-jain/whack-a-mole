4bba95cec80316c3baa8a6338eccec4a6dea95a7
jenish jain <jenishjain@rocketmail.com>
2019-10-15 10:31:09 +0530

    Whack a mole - v2
    The game is now handled by event listeners and can be expanded any time by adding more divs in the index.html section with class="holes" without any change in CSS or javascript.  
    also now the game speed increased gradually as you score high and a high score feature has been added.
    at the end, an alert is displayed with your score.
    and instead of being time-constrained now clicking the wrong hole ends the game.
    
    [scopes of imporvements]
    
    prevent double click on the same hole.
    style alert box
    add animations and styling to whole game.

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
+var holeId;
+var pickedhole;
+var gameTime = 0;
+var currentScore = 0;
+var highScore = 0;
+var newMoleTime = 1000; // time for a new mole to come out
+var OutTime = 0.5 * newMoleTime;
 
-let moleId = 0;
-// let state = 0;
-let id;
-let gameTime=60;
-let currentScore = 0;
+var hole = document.getElementsByClassName("holes")
+console.log(hole);
+var numberOfHoles = hole.length;
+console.log(numberOfHoles);
 
 
-function moleout(){
-     moleId = picker();
-    //  state = 1;  //  state one signifies mole is outside hole
-   console.log(moleId);
-// console.log('state :',state, 'moleId :', moleId);
-    var mole = document.getElementById(moleId);
-    mole.style.background = "#a23131";
-    mole.innerText= "🦔"
-    moleOutTime = setInterval(refresh,800);
-
-    gameTime--;
-    document.getElementById("clock").innerText= gameTime;
-    if(gameTime==0){
-        clearInterval(timer);
-        document.getElementById('startbtn').disabled = false;
-    }
-
-}
-function picker(){
-var pickedNum = Math.floor(Math.random() * 9)+1;
-return pickedNum;
+function holepicker() {
+    pickedhole = Math.floor(Math.random() * numberOfHoles);
+    return pickedhole;
 }
 
-function refresh (){
-    // state = 0;  //  state one signifies mole is inside hole
-    // console.log('state :',state, 'moleId :', moleId);
-    var mole = document.getElementById(moleId);
-    mole.style.background = "#a1dd70";
-    mole.innerText=" ";
-    clearInterval(moleOutTime);
-    
-}
-
-
-function startGame(){
-    timer = setInterval(moleout,1000);
-    gameTime=60;
-    currentScore=0;
+function startGame() {
+    timer = setInterval(moleout, newMoleTime);
+    gameTime = 0;
+    currentScore = 0;
+    document.getElementById('clock').innerText = gameTime;
+    document.getElementById('currentScore').innerText =currentScore
     document.getElementById('startbtn').disabled = true;
 }
 
-function hole(i){
-    if(i == moleId){
+function moleout() {
+    holepicker();
+    hole[pickedhole].style.background = "#a23131";
+    hole[pickedhole].innerText = "🦔";
+    gameTime++;
+    document.getElementById('clock').innerText = gameTime;
+    moleOutTime = setInterval(refresh, OutTime)
+}
+
+function refresh() {
+    hole[pickedhole].style.background = "#a1dd70";
+    hole[pickedhole].innerText = " ";
+    clearInterval(moleOutTime);
+
+}
+
+function whackCheck(event) {
+    // if target event is same as picked hole increase score
+    if (event.target == hole[pickedhole]) {
         currentScore++;
-    document.getElementById("currentScore").innerText= currentScore;
+        document.getElementById("currentScore").innerText = currentScore;
+
+        if(newMoleTime >100){
+            newMoleTime = 1000 - Math.floor(currentScore/20)*50;
+            console.log(newMoleTime);       // the game becoemes gradually tougher as you score higher
+        }
+        
     }
+    else {
+        clearInterval(timer);
+        gameTime = 0;
+        document.getElementById('startbtn').disabled = false;
+        alert("GAME OVER  \n your score is " + currentScore);
+
+        if(currentScore > highScore)
+        {
+            document.getElementById('highScore').innerText = currentScore;// current score is high score
+        }
+    }
+}
+// adding event listner to class holes
+for (holeId = 0; holeId < numberOfHoles; holeId++) {
+    var currentHole = hole[holeId];
+    currentHole.addEventListener('click', whackCheck);
 }
